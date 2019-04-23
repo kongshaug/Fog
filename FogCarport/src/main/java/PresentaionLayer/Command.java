67023fd339d508c758791b8370c14fbb5913ca4d
kongshaug <benja_kongs@hotmail.com>
2019-04-23 10:20:22 +0200

    layer setup

---
+++ FogCarport/src/main/java/PresentaionLayer/Command.java
@@ -0,0 +1,28 @@
+package PresentaionLayer;
+
+import java.util.HashMap;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+abstract class Command {
+
+    private static HashMap<String, Command> commands;
+
+    private static void initCommands() {
+        commands = new HashMap<>();
+  //      commands.put( "login", new CMD_Login() );
+
+    }
+
+    static Command from( HttpServletRequest request ) {
+        String commandName = request.getParameter( "command" );
+        if ( commands == null ) {
+            initCommands();
+        }
+        return commands.getOrDefault(commandName, new CMD_UnknownCommand() );
+    }
+
+    abstract String execute( HttpServletRequest request, HttpServletResponse response ) 
+            throws Exception;
+
+}
