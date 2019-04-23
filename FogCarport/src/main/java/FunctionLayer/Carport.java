f4b7fd086d9f7cbbfc31ef51429ef34222e3f8e1
kongshaug <benja_kongs@hotmail.com>
2019-04-23 12:27:15 +0200

    calculate

--- FogCarport/src/main/java/FunctionLayer/Carport.java
+++ FogCarport/src/main/java/FunctionLayer/Carport.java
@@ -5,11 +5,59 @@
  */
 package FunctionLayer;
 
+
+import java.util.HashMap;
+
+
+
 /**
  *
  * @author sofieamalielandt
  */
 public class Carport
 {
+    private int length;
+    private int hight;
+    private HashMap<String, HashMap<String, String>> materials;
+    private roof roof;
+    private shed shed;
+
+    public Carport(int length, int hight, roof roof, shed shed) {
+        this.length = length;
+        this.hight = hight;
+        this.roof = roof;
+        this.shed = shed;
+    }
+        public Carport(int length, int hight, roof roof) {
+        this.length = length;
+        this.hight = hight;
+        this.roof = roof;
+       
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public int getHight() {
+        return hight;
+    }
+
+    public HashMap<String, HashMap<String, String>> getMaterials() {
+        return materials;
+    }
+
+    public roof getRoof() {
+        return roof;
+    }
+
+    public shed getShed() {
+        return shed;
+    }
+
+    public void setMaterials(HashMap<String, HashMap<String, String>> materials) {
+        this.materials = materials;
+    }
+    
     
 }
